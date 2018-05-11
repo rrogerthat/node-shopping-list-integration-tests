c4c382ab2a3a8e483ca955d573bd68e67a6622f0
Roger Hwang <hroger19@gmail.com>
2018-05-11 15:33:47 -0700

    created new branch

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ let server;
 // this function starts our server and returns a Promise.
 // In our test code, we need a way of asynchronously starting
 // our server, since we'll be dealing with promises there.
-function runServer() {
+function runServer() {                                     //Purpose: Promise makes sure server starts before tests start.
   const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
+  return new Promise((resolve, reject) => {                //need return?
+    server = app.listen(port, () => {                      //starts server and listens to port just like in express apps
       console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+      resolve(server);                                     //In promise, if console.log goes through test, resolve then actually runs server.
+    }).on('error', err => {                                //If event returns error, run reject (part of Promise keywords)
+      reject(err)                                          
     });
   });
 }
@@ -47,24 +47,28 @@ function runServer() {
 // like `runServer`, this function also needs to return a promise.
 // `server.close` does not return a promise on its own, so we manually
 // create one.
-function closeServer() {
+function closeServer() {                                //Ran after CRUD tests??
   return new Promise((resolve, reject) => {
     console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
+    server.close(err => {                               //node.js: The server.close() method stops the HTTP server from accepting new connections.
+      if (err) {                                        //With callback: Specifies a function to be executed after the server stops listening for connections
+        reject(err);                                    //It will be called with an Error as its only argument if the server was not open when it was closed.
         // so we don't also call `resolve()`
         return;
       }
-      resolve();
+      resolve();                                        //resolve() is empty?
     });
   });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
+if (require.main === module) {  //When a file is run directly from Node.js, require.main is set to its module. 
+  //That means that it is possible to determine whether a file has been run directly by testing require.main === module. Ex. thru node server.js
+  //When running npm test, mocha looks for .js file in test folder to run.
+  runServer().catch(err => console.error(err)); //Under Promise instance, .catch runs if there was a reject in Promise (error)- since server already running??
+  //if ran thru node, still be able to run test code thru ln 70.
+  //When we open this file in order to import app and runServer in a test module, we don't want the server to automatically run, and this conditional block makes that possible.
 };
 
 module.exports = {app, runServer, closeServer};
--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('../server');
+const {app, runServer, closeServer} = require('../server');     //calarify app??
 
 // this lets us use *expect* style syntax in our tests
 // so we can do things like `expect(1 + 1).to.equal(2);`
 // http://chaijs.com/api/bdd/
-const expect = chai.expect;
+const expect = chai.expect;                 //or above can do: const expect = require('chai').expect;
 
 // This let's us make HTTP requests
 // in our tests.
@@ -17,12 +17,12 @@ chai.use(chaiHttp);
 describe('Shopping List', function() {
 
   // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
+  // function returns a promise, and we return that promise by
   // doing `return runServer`. If we didn't return a promise here,
   // there's a possibility of a race condition where our tests start
   // running before our server has started.
   before(function() {
-    return runServer();
+    return runServer();                                           //why return?
   });
 
   // although we only have one test module at the moment, we'll
@@ -42,11 +42,11 @@ describe('Shopping List', function() {
     // for Mocha tests, when we're dealing with asynchronous operations,
     // we must either return a Promise object or else call a `done` callback
     // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
-    return chai.request(app)
-      .get('/shopping-list')
-      .then(function(res) {
-        expect(res).to.have.status(200);
+    // and returns a Promise, so we just return it.              //Request we make with the Chai HTTP test client is asynchronous and returns a promise?
+    return chai.request(app)                                     //When passing an app to request, it will automatically open the server for incoming requests
+      .get('/shopping-list')                                     //In order to use app.get, app.post, etc. from server.js (under Chai-http module)
+      .then(function(res) {                                      //.then runs after request complete
+        expect(res).to.have.status(200);                         //Test inspects the response object
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
 
